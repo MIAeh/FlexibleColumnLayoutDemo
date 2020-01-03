355c250937343d9abe357a3c49bd4c3fbc1ad8c0
EdStrickland <2499128545@qq.com>
2020-01-03 11:06:33 +0800

    Update Master.controller.js

--- webapp/controller/Master.controller.js
+++ webapp/controller/Master.controller.js
@@ -11,9 +11,9 @@ sap.ui.define([
 
 	return Controller.extend("sap.f.FlexibleColumnLayoutWithOneColumnStart.controller.Master", {
 		onInit: function () {
+			this.getView().setModel(new JSONModel(this.sortedData()), "model");
 			this.oRouter = this.getOwnerComponent().getRouter();
 		},
-
 		onItemPress: function (oEvent) {
 			var oNextUIState = this.getOwnerComponent().getHelper().getNextUIState(1),
 				path = oEvent.getSource().getBindingContext("fieldTypes").getPath(),
@@ -22,7 +22,6 @@ sap.ui.define([
 
 			this.oRouter.navTo("detail", {layout: oNextUIState.layout, fieldNumber: itemNumber});
 		},
-
 		getGroup: function (oContext) {
 			var oElementContext = oContext.getProperty("elements/0/context");
 			var key, text;
@@ -38,6 +37,20 @@ sap.ui.define([
 				key: key,
 				text: text
 			}
+		},
+		sortedData: function() {
+			var original = this.getOwnerComponent().getModel("fieldTypes").getData();
+			var itemTypes = original.itemTypes;
+			original.itemTypes = this.sortItemTypes(itemTypes);
+			return original;
+		},
+		sortItemTypes: function(itemTypes) {
+			return itemTypes.sort(function(a, b) {
+				if (a.elements[0].context === "CoreModel")
+					return -1;
+				else
+					return 1;
+			});
 		}
 	});
 }, true);
